72f493f00c8f36079ddbf1d2f97bd383797d63af
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-04-03 13:37:35 +0900

    add: shoptype, location model page added

--- shop/admin.py
+++ shop/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import ShopData
-from .models import Comments
+from .models import ShopData, Comments, ShopType, Location
 
 
 class ShopDataAdmin(admin.ModelAdmin):
     search_fields = ['Name']
-
 # Register your models here.
 
 
 admin.site.register(ShopData, ShopDataAdmin)
+admin.site.register(ShopType)
 admin.site.register(Comments)
+admin.site.register(Location)
