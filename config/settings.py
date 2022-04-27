9af3ce07a2e82e30a414715ee000f55a1283f959
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-04-27 12:26:43 +0900

    add: django authtoken

--- config/settings.py
+++ config/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'rest_framework',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
 
