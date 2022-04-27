630b37572c5dff960119cd4ca61e4ab95c6bcdeb
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-04-27 12:14:01 +0900

    edit: autopep8 applied

--- shop/models.py
+++ shop/models.py
@@ -3,22 +3,28 @@ from django.db import models
 from django.forms import ImageField
 
 # Create your models here.
+
+
 class ShopType(models.Model):
     type = models.CharField(max_length=50, help_text="매장 종류를 입력(예: 서점)")
+
     def __str__(self) -> str:
         """
         Display title instead of ID
         """
         return self.type
-    
+
+
 class Location(models.Model):
     location = models.CharField(max_length=50, help_text="지역 입력(예: 서울)")
+
     def __str__(self) -> str:
         """
         Display title instead of ID
         """
         return self.location
-    
+
+
 class ShopData(models.Model):
     '''Contained info of ShopData
         * shopname
@@ -30,22 +36,45 @@ class ShopData(models.Model):
         * shop homepage
         * shop photo
     '''
-    name = models.CharField(max_length=100, help_text="매장 이름 입력")   # 매장 이름
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True) # 매장 지역
-    address = models.CharField(max_length=200, help_text="매장 상세주소 입력") # 매장 상세주소
+    name = models.CharField(
+        max_length=100,
+        help_text="매장 이름 입력"
+    )   # 매장 이름
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True
+    )  # 매장 지역
+    address = models.CharField(
+        max_length=200,
+        help_text="매장 상세주소 입력"
+    )  # 매장 상세주소
     workTime = models.TextField(help_text="매장 운영시간 입력")  # 매장 운영시간
     contact = models.TextField(help_text="매장 연락처 입력")  # 매장 연락처
     content = models.TextField(help_text="매장 상세정보 입력")   # 매장 상세정보
     shopType = models.ManyToManyField(ShopType, help_text="매장 종류 선택", )  # 매장종류
-    homePage = models.CharField(max_length=100, blank=True, null=True) # 매장 홈페이지주소
-    twitter = models.CharField(max_length=100, blank=True, null=True) # 매장 트위터주소
-    photo = models.ImageField(blank=True, null=True, upload_to='%Y%m%d%m')  # 매장 사진
+    homePage = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )  # 매장 홈페이지주소
+    twitter = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )  # 매장 트위터주소
+    photo = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to='%Y%m%d%m'
+    )  # 매장 사진
 
     def __str__(self) -> str:
         """
         Display title instead of ID
         """
         return self.name
+
 
 class Comment(models.Model):
     '''Comments of ShopData
@@ -56,7 +85,8 @@ class Comment(models.Model):
     nickName = models.CharField(max_length=100)  # 닉네임
     content = models.TextField()   # 댓글 내용
     date = models.DateTimeField()   # 댓글 쓴 날짜
-    shop = models.ForeignKey(ShopData, on_delete=models.CASCADE, null=True) # 댓글을 달 샵 정보
+    shop = models.ForeignKey(
+        ShopData, on_delete=models.CASCADE, null=True)  # 댓글을 달 샵 정보
 
     def __str__(self) -> str:
         """
