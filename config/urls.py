fe94dcd30924c0ef540037028f3a94f97dc38217
LeeDongHyeong <dlehdgud2380@gmail.com>
2022-12-27 14:05:05 +0900

    edit: endpoint path edited

--- config/urls.py
+++ config/urls.py
@@ -42,12 +42,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('v1/shop/', views.ShopList.as_view()),
+    path('v1/shop/<int:id>', views.ShopInfo.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
-        path('v1/shop/', views.ShopList.as_view()),
-        path('v1/shop/<int:id>', views.ShopInfo.as_view()),
         re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
         re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
         re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
