e457e0cb032aa662a06ce24675ca3d721f0eae38
LeeDongHyeong <dlehdgud2380@gmail.com>
2023-07-16 19:29:26 +0900

    fix: whitenoise no module named fix

--- otaroad/settings/base.py
+++ otaroad/settings/base.py
@@ -80,8 +80,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    '"whitenoise.middleware.WhiteNoiseMiddleware"'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'otaroad.urls'
 
