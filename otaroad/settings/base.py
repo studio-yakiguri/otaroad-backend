09f5a4d2da6359fc149caef5a7ccab7e0d51eb94
LeeDongHyeong <dlehdgud2380@gmail.com>
2023-07-17 19:07:00 +0900

    fix: Vmimetypes.add_type 'text/html'

--- otaroad/settings/base.py
+++ otaroad/settings/base.py
@@ -24,6 +24,7 @@ import mimetypes
 
 # CSS Broken FIX
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/html", ".html", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
