ca562a45c18dc63d31510ca1a65a37aa78c316c5
LeeDongHyeong <dlehdgud2380@gmail.com>
2023-07-17 18:41:41 +0900

    update: swagger api info edited

--- otaroad/urls.py
+++ otaroad/urls.py
@@ -29,26 +29,26 @@ from shop import views
 # Swegger Setting
 schema_view = get_schema_view(
     openapi.Info(
-        title="Subkaru-Map API",
+        title="Otaroad API",
         default_version='v1',
-        description="Test description",
+        description="Otaroad Map API",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="dlehdgud2380@gmail.com"),
         license=openapi.License(name="No License"),
     ),
     public=True,
-    permission_classes=[permissions.AllowAny],
+    permission_classes=[permissions.IsAdminUser],
 )
 
+# URL Setting
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/shop/', views.ShopList.as_view()),
     path('v1/shop/<int:id>', views.ShopInfo.as_view()),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    ]
