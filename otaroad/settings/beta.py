2341b581437c94ef844e2574826dcbaa88c57e9b
LeeDongHyeong <dlehdgud2380@gmail.com>
2024-07-11 20:22:30 +0900

    fix: CSRF COR ORIGINS addr ip edited

--- otaroad/settings/beta.py
+++ otaroad/settings/beta.py
@@ -1,8 +1,11 @@
 from .base import *
 from .util import get_secure_data
 
-ALLOWED_HOSTS = ["127.0.0.1", "100.88.98.138",
-                 "otaroad-oracle-cloud.wahoo-in.ts.net", "beta.otaroad.party"
+SERVER_TAILSCALE_IP = '100.109.210.96'
+SERVER_RELEASE_ADDR = 'otaroad-oracle-cloud.wahoo-in.ts.net'
+
+ALLOWED_HOSTS = ["127.0.0.1", SERVER_TAILSCALE_IP,
+                 SERVER_RELEASE_ADDR, "beta.otaroad.party"
                  ]
 
 # Debug Options
@@ -32,20 +35,24 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "https://dev.subculture-map-frontend.pages.dev",
     "http://127.0.0.1:3000",
-    "http://100.88.98.138:7500",
+    f"http://{SERVER_TAILSCALE_IP}:7500",
     "https://beta.otaroad.party",
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net",
+    SERVER_RELEASE_ADDR,
 ]
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # 신뢰할 수 있는 ORIGIN 추가
 CSRF_TRUSTED_ORIGINS = [
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net/v1/shop/",
+    f"{SERVER_RELEASE_ADDR}/v1/shop/",
     "https://beta.otaroad.party",
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net/swagger",
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net/redoc",
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net/otaroad-admin/",
-    "https://otaroad-oracle-cloud.wahoo-in.ts.net/django-admin/",
+    f"{SERVER_RELEASE_ADDR}/swagger",
+    f"{SERVER_RELEASE_ADDR}/redoc",
+    f"{SERVER_RELEASE_ADDR}/otaroad-admin/",
+    f"{SERVER_RELEASE_ADDR}/django-admin/",
     "http://127.0.0.1:3000",
+    f"http://{SERVER_TAILSCALE_IP}:7500/otaroad-admin/",
+    f"http://{SERVER_TAILSCALE_IP}:7500/django-admin/",
+    f"http://{SERVER_TAILSCALE_IP}/swagger",
+    f"http://{SERVER_TAILSCALE_IP}/redoc",
 ]
