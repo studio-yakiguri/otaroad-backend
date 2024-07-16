f3b1429cd099ab31f8062686f4ac133e7188b2d7
LeeDongHyeong <dlehdgud2380@gmail.com>
2024-07-16 23:42:37 +0900

    fix: backend url

--- static/js/otaroad-admin-request.js
+++ static/js/otaroad-admin-request.js
@@ -1,7 +1,7 @@
 /* otaroad.admin.request.js by LeeDongHyeong */
 
 // 백엔드 서버 주소 설정
-const url = "https://127.0.0.1:7500";
+const url = "https://127.0.0.1:7500/shop/v1";
 
 // 매장리스트 MAP
 let shopList = new Map();
